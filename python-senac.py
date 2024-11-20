cd79a61bd0b8f867710d9c822efa3758b767edf6
Shaka Nken <72205556+nkenshaka@users.noreply.github.com>
2024-11-20 15:22:35 +0000

    sim

--- python-senac.py
+++ python-senac.py
@@ -1,1 +1,22 @@
+import random
 
+print('Bem vindo ao Gerador de senhas!')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+[]|;:,.<>?/0123456789'
+
+number = input('Quantas senhas precisa?: ')
+number = int(number)
+
+length = input('Qual o comprimento da senha: ')
+length = int(length)
+
+if number > 1:
+    print('\nAqui estão suas senhas: ')
+else:
+    print('\nAqui está sua senha: ')
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
